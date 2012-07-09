6a198ea4cb54cc827415b9e45758867e8ede52ec
Deepak Subramanian <deepaks@ischool.berkeley.edu>
2012-07-08 23:25:50 -0700

    Initial import

---
+++ deepak-python.el
@@ -0,0 +1,4 @@
+;;Deepak Python Support for emacs begin
+(add-to-list 'load-path "~/.emacs.d/python/python-mode") 
+(setq py-install-directory "~/.emacs.d/python/python-mode")
+(require 'python-mode)
