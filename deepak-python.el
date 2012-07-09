397aa3bef2bffd32c49e0d6199976bcc53f47782
Deepak Subramanian <deepaks@ischool.berkeley.edu>
2012-07-08 23:57:16 -0700

    Added Ipython python mode readline

--- deepak-python.el
+++ deepak-python.el
@@ -1,4 +1,10 @@
+
 ;;Deepak Python Support for emacs begin
 (add-to-list 'load-path "~/.emacs.d/python/python-mode") 
 (setq py-install-directory "~/.emacs.d/python/python-mode")
 (require 'python-mode)
+(add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
+
+;;Deepak Support for Ipython
+(add-to-list 'load-path "~/.emacs.d/python") 
+(require 'ipython)
