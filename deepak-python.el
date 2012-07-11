8bc241fe842469ee35ace696a910f597d8987813
Deepak Subramanian <deepaks@ischool.berkeley.edu>
2012-07-11 08:52:38 -0700

    Color theme and python modes are now working. Python shell is still not perfected

--- deepak-python.el
+++ deepak-python.el
@@ -1,4 +1,4 @@
-
+;;ewjdsfjsdjfdflkja;sdfjk
 ;;Deepak Python Support for emacs begin
 (add-to-list 'load-path "~/.emacs.d/python/python-mode") 
 (setq py-install-directory "~/.emacs.d/python/python-mode")
