23f3a37b1c285014104346c01920d5b2c98c3c12
Deepak Subramanian <deepaks@ischool.berkeley.edu>
2012-07-27 14:56:43 -0700

    Working with org mode and ipython

---
+++ deepak-js.el
@@ -0,0 +1,2 @@
+;; This is the mode which will support both Javascript and JSON mode
+(require 'json-mode)
--- deepak-python.el
+++ deepak-python.el
@@ -4,7 +4,24 @@
 (setq py-install-directory "~/.emacs.d/python/python-mode")
 (require 'python-mode)
 (add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
+(add-to-list 'interpreter-mode-alist '("python" . python-mode))
 
 ;;Deepak Support for Ipython
-(add-to-list 'load-path "~/.emacs.d/python") 
+(add-to-list 'load-path "~/.emacs.d/python")
 (require 'ipython)
+(setq ipython-command "/.emacs.d/python/ipython")
+
+
+;;Support for autocomplete
+(require 'anything)
+(require 'anything-ipython)
+(when (require 'anything-show-completion nil t)
+   (use-anything-show-completion 'anything-ipython-complete
+                                 '(length initial-pattern)))
+
+;;Deal with interpreter
+(require 'comint)
+(define-key comint-mode-map (kbd "M-") 'comint-next-input)
+(define-key comint-mode-map (kbd "M-") 'comint-previous-input)
+(define-key comint-mode-map [down] 'comint-next-matching-input-from-input)
+(define-key comint-mode-map [up] 'comint-previous-matching-input-from-input)
